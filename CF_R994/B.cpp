d7a78959ad4a524377ed8b03ce32071135316553
FeiYehua <31472675+feiyehua@users.noreply.github.com>
2024-12-20 23:54:15 +0800

    B

--- CF_R994/B.cpp
+++ CF_R994/B.cpp
@@ -1,7 +1,7 @@
 /*** 
  * @Author       : FeiYehua
  * @Date         : 2024-12-20 22:27:27
- * @LastEditTime : 2024-12-20 22:27:29
+ * @LastEditTime : 2024-12-20 23:06:26
  * @LastEditors  : FeiYehua
  * @Description  : 
  * @FilePath     : B.cpp
@@ -10,3 +10,46 @@
 #include<bits/stdc++.h>
 using namespace std;
 int t;
+int n;
+string s;
+bool flagS;
+bool flagP;
+bool flagCan;
+int main()
+{
+    cin>>t;
+    for(int i=1;i<=t;i++)
+    {
+        cin>>n;
+        cin>>s;
+        flagS=1;
+        flagP=1;
+        flagCan=1;
+        if(s[0]=='s') s[0]='.';
+        if(s[s.size()-1]=='p') s[s.size()-1]='.';
+        for(int j=0;j<s.size();j++)
+        {
+            if(flagP==0&&flagS==0&&s[j]=='s')
+            {
+                flagCan=0;
+            }
+            if(s[j]=='s')
+            {
+                flagS=0;
+            }
+            else if(s[j]=='p')
+            {
+                flagP=0;
+            }
+        }
+        //如果只有sp中一种字符，显然是可以的
+        if((!(flagS==0&&flagP==0)))
+        {
+            cout<<"YES\n";
+        }
+        else
+        {
+            cout<<"no\n";
+        }
+    }
+}
